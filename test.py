f383f090d5f693c05d08c5e5b4df571bff257098
Dylan <77236413+Ling-Lang@users.noreply.github.com>
2022-10-17 22:56:50 +0200

    Changed a few things

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import discord
 
 bot = discord.Bot()
-
+b_name = "@testing_bot"
 @bot.slash_command()
 async def hi(ctx, name: str = None):
     name = name or ctx.author.name
@@ -9,6 +9,6 @@ async def hi(ctx, name: str = None):
 
 @bot.user_command(name="Say Hello")
 async def hi(ctx, user):
-    await ctx.respond(f"{ctx.author.mention} says hello to {ctx.author.name}!")
+    await ctx.respond(f"{b_name} says hello to {ctx.author.mention}!")
 
 bot.run("MTAyNzI5OTAzNzI0Mzc3Mjk5OA.GWjZe7.ygoC7BU7vEt_omJWA8PJBZXAhGPMRWFUMTFeI4")
