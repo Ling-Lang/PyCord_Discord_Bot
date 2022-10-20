b2947458d1af5e16a0e75e166c8ae2709e4ed2d7
Ling-Lang <kulkajens82@gmial.com>
2022-10-20 17:30:52 +0200

    Added more buttons with functionality

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-from code import interact
 import discord
 from paramiko import SSHClient
 import requests
@@ -66,37 +65,82 @@ async def online(ctx):
         await ctx.respond(f"Server is online!")
         is_online = True
     else:
-        await ctx.respond(f"Offline")
+        await ctx.respond(f"Server is offline!")
     
 @bot.command()
 async def test(ctx):
-    button = discord.ui.Button(label="Click me!", style=discord.ButtonStyle.primary, emoji="<:herr_goetter:945325132480643132>")
-
+    button = discord.ui.Button(label="Connect to server", style=discord.ButtonStyle.primary, emoji="<:herr_goetter:945325132480643132>")
+    button2 = discord.ui.Button(label="Start Server", style=discord.ButtonStyle.primary, emoji="<:GigaChad:882287966951723068>")
+    button3 = discord.ui.Button(label="Disconnect from server", style=discord.ButtonStyle.primary, emoji="<a:peepoExit:662162946667053056>")
+    button4 = discord.ui.Button(label="Check if online", style=discord.ButtonStyle.primary, emoji="<a:x_Loading:737676088339202086>")
+    button5 = discord.ui.Button(label="Player Count", style=discord.ButtonStyle.primary, emoji="<:sgpeople:1003299502834319360>")
+    # button6 = discord.ui.Button(label="Stop Server", style=discord.ButtonStyle.primary, emoji="<:stop:980007702698799115>")
     async def callback(interaction: discord.Interaction):
-        await interaction.response.send_message("Connecting to server!", ephemeral=True)
-        connecttoserver()
-        await interaction.response.edit_message("Connected:  {out}")
-    
+        await interaction.response.send_message("Connecting to server!", ephemeral=False)
+        await connecttoserver()
+    async def callback2(interaction: discord.Interaction):
+        await interaction.response.send_message("Starting Server!", ephemeral=False)
+        startserver()
+    async def callback3(interaction: discord.Interaction):
+        await interaction.response.send_message("Disconnecting from server!", ephemeral=False)
+        await disonnectfromserver(ctx)
+    async def callback4(interaction: discord.Interaction):
+        await interaction.response.send_message("Checking if online!", ephemeral=False)
+        await checkonline(ctx)
+    async def callback5(interaction: discord.Interaction):
+        await interaction.response.send_message("Getting player count!", ephemeral=False)
+        await playercount(ctx)
+    # async def callback6(interaction: discord.Interaction):
+    #     await interaction.response.send_message("Stopping Server!", ephemeral=False)
+    #     await stopserver(ctx)
     button.callback = callback
-    view = discord.ui.View()
-    view.add_item(button)
+    button2.callback = callback2
+    button3.callback = callback3
+    button4.callback = callback4
+    button5.callback = callback5
+    # button6.callback = callback6
+    view = discord.ui.View(button, button2, button3, button4, button5)
     await ctx.send("test" , view=view)
-out = "string"
-def connecttoserver():
+
+async def connecttoserver(ctx):
     client.load_system_host_keys()
     client.connect('play.dylanderechte.online', username="root", password="Dylan@Server")
-    stdin, stdout, stderr = client.exec_command('cd ~; ls')
+    # stdin, stdout, stderr = client.exec_command('cd ~; ls')
+    # out = stdout.read().decode("utf8")
+    # print(out)
+    #await ctx.respond(f"Connected to the Server!")
+    #stdin, stdout, stderr = client.exec_command('tmux new -s server')
+
+def startserver():
+    #stdin, stdout, stderr = client.exec_command('tmux new -s server | neofetch')
+    stdin, stdout, stderr = client.exec_command('cd ~')
+    stdin, stdout, stderr = client.exec_command('./test.sh')
     out = stdout.read().decode("utf8")
     print(out)
 
+async def disonnectfromserver(ctx):
+    stdin, stdout, stderr = client.exec_command('tmux detach')
+    stdin, stdout, stderr = client.exec_command('exit')
+    await ctx.respond(f"Disconnected from Server!")
+    client.close()
 
+async def checkonline(ctx):
+    response = requests.get('https://api.mcsrvstat.us/2/play.dylanderechte.online')
+    if response.json()['online'] == True:
+       await ctx.respond(f"Server is online!")
+       is_online = True
+    else:
+        await ctx.respond(f"Server is offline!")
 
+async def playercount(ctx):
+    response = requests.get('https://api.mcsrvstat.us/2/play.dylanderechte.online')
+    res = response.json()['players']['online']
+    await ctx.respond(f"There are {res} players online!")
 
-
-
-
-
-
+# async def stopserver(ctx):
+#     stdin, stdout, stderr = client.exec_command('screen -r')
+#     stdin, stdout, stderr = client.exec_command('stop')
+#     await ctx.respond(f"Server stopped!")
 
 
 
